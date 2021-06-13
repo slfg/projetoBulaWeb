dc7c120d095e21b62ce4adc3f57bf85782ce6d83
unknown <sgleft@gmail.com>
2021-06-13 16:26:30 -0300

    backend e frontend integrados

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = 'https://api.fda.gov/drug/drugsfda.json?search=products.active_ingredients.name:'
+const url = 'http://localhost:8080/meds/princ/'
 
 const searchInput = getElement('.form-control'),
     searchButton = getElement('.btn.btn-primary'), // Essa parte do código é qual a classe
@@ -11,31 +11,7 @@ let element = document.getElementById('info')
 var medic,
     datamed,
     card,
-    idioma = {
-        eng: {
-            titulo: "Digital Medicine Leaflet",
-            intro: "The digital medicine leaflet is designed to simplify and share the most relevant information of medications in a simple way and easy to understand!",
-            searchbutton: "Search",
-            searchinput: "Insert the medication name",
-            error: "Nothing found",
-            dropdownMenuLink: "Language"
-        }
-    },
-    f5 = document.querySelectorAll("[f5]");
-
-if (window.location.hash) {
-    if (window.location.hash === "#eng") {
-        intro.textContent = idioma.eng.intro
-        titulo.textContent = idioma.eng.titulo
-        searchButton.textContent = idioma.eng.searchbutton
-        searchinput.placeholder = idioma.eng.searchinput
-        dropdownMenuLink.textContent = idioma.eng.dropdownMenuLink
-    }
-}
-
-window.onhashchange = function() {
-    location.reload();
-};
+    num = 0;
 
 function getElement(element) {
     return document.querySelector(element);
@@ -43,84 +19,49 @@ function getElement(element) {
 
 function requestMedInfo(url, name) {
     fetch(url + name)
-        .then(response => response.json()) //Essa parte é a parte que faz a chamada do
-        .then(data => { //Input do usuário para a API
+        .then(response => response.json())
+        .then(data => {
             datamed = data;
         })
         .catch(err => console.log(err));
 }
 
-function createCard() { // Essa parte é onde é postado no HTML as informações do remédio, como podem ver é uma mistura de html com javascript (html para a formatação, js para mostrar as informaçÕes)
-    if (datamed.results == null) {
-        if (window.location.hash) {
-            if (window.location.hash === "#eng") {
-                element.innerHTML = `
-            <div class="boxInfo"><span id="just-line-break" style="font-size:30px">⚠\n</span><span id="error" style="font-family:Montserrat-Bold">Nothing Found</span></div>`
-            } else {
-                element.innerHTML = `
-            <div class="boxInfo"><span id="just-line-break" style="font-size:30px">⚠\n</span><span id="error" style="font-family:Montserrat-Bold">Nada encontrado</span></div>`
-            }
-        } else {
-            element.innerHTML = `
-        <div class="boxInfo"><span id="just-line-break" style="font-size:30px">⚠\n</span><span id="error" style="font-family:Montserrat-Bold">Nada encontrado</span></div>`
-        }
+function createCard() {
+    element.innerHTML = `
+                <div class="boxInfo">
+                <span id="just-line-break" style="font-family:Montserrat-Bold"> Produto: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat">${datamed[num].prodWithConc} \n \n</span>
+                <span id="just-line-break" style="font-family:Montserrat-Bold"> Apresentação: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat">${datamed[num].tx_apres} \n \n</span>  
+                <span id="just-line-break" style="font-family:Montserrat-Bold"> Classe terapêutica: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat">${datamed[num].tx_class_terap} \n \n</span>
+                <span id="just-line-break" style="font-family:Montserrat-Bold"> Tarja: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat">${datamed[num].tx_tarja} \n \n</span>
+                <button class="borderlessButton" onclick='prevMed()'>◀</button>
+                <span id="just-line-break" style="font-family:Montserrat">${num}</span>
+                <button class="borderlessButton" onclick='num++; createCard()'>▶</button>`
+    return card;
+}
+
+function prevMed() {
+    if (num >= 0) {
+        num--;
+        createCard()
     } else {
-        if (window.location.hash) {
-            if (window.location.hash === "#eng") {
-                element.innerHTML = `
-            <div class="boxInfo">
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Brand Name: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].brand_name} \n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold">Marketing Status: \n</span> 
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].marketing_status}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Strength of Medication: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].active_ingredients[0].strength}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Route of Medication: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].route}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Dosage Form: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].dosage_form} \n</span></div>`;
-                return card;
-            } else {
-                element.innerHTML = `
-            <div class="boxInfo">
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Nome Comercial: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].brand_name} \n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold">Status comercial: \n</span> 
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].marketing_status}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Força da medicação: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].active_ingredients[0].strength}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Forma de ingerir: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].route}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Forma de dosagem: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].dosage_form} \n</span></div>`;
-            }
-        } else {
-            element.innerHTML = `
-            <div class="boxInfo">
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Nome Comercial: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].brand_name} \n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold">Status comercial: \n</span> 
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].marketing_status}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Força da medicação: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].active_ingredients[0].strength}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Forma de ingerir: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].route}\n \n </span>
-            <span id="just-line-break" style="font-family:Montserrat-Bold"> Forma de dosagem: \n</span>
-            <span id="just-line-break" style="font-family:Montserrat"> ${datamed.results[0].products[0].dosage_form} \n</span></div>`;
-        }
+        num = 0;
     }
 }
 
 function startApp(medic) {
     requestMedInfo(url, medic);
 
-    setTimeout(function() { // Eu criei essa parte apenas para dar um timeout
+    setTimeout(function() {
         container.innerHTML = createCard();
-    }, 2000);
+    }, 1000);
 }
 
 searchButton.addEventListener('click', event => {
-    event.preventDefault(); //Essa parte é responsável pela funcionalidade do botão search
+    event.preventDefault();
     medic = searchInput.value.toLowerCase();
     startApp(medic);
 });
