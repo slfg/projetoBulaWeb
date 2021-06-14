23fdbb988c7a776b3603b1ea5b75ca78795ad3d0
unknown <sgleft@gmail.com>
2021-06-13 22:21:44 -0300

    novo layout

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const url = 'http://localhost:8080/meds/princ/'
 
-const searchInput = getElement('.form-control'),
-    searchButton = getElement('.btn.btn-primary'), // Essa parte do código é qual a classe
-    container = getElement('.datamed'), // que o backend (no caso esse arquivo) vai ler para enviar para a API
-    erroMessage = getElement('.error');
+const
+    searchButton = getElement('.search-btn'), // Essa parte do código é qual a classe
+    container = getElement('.datamed'); // que o backend (no caso esse arquivo) vai ler para enviar para a API
 
 
 let element = document.getElementById('info')
+let searchInput = document.getElementById('searchinput')
 
 var medic,
     datamed,
@@ -23,23 +23,28 @@ function requestMedInfo(url, name) {
         .then(data => {
             datamed = data;
         })
-        .catch(err => console.log(err));
+        .catch(err => alert("Por favor insira um principio ativo"));
 }
 
 function createCard() {
-    element.innerHTML = `
+    if (datamed[num] == null) {
+        element.innerHTML = `<div class="boxInfo">
+        <span id="just-line-break" style="font-family:Montserrat-Bold">❌\n Nada encontrado</span>`
+    } else {
+        element.innerHTML = `
                 <div class="boxInfo">
-                <span id="just-line-break" style="font-family:Montserrat-Bold"> Produto: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat-Bold">✓ Produto: \n</span>
                 <span id="just-line-break" style="font-family:Montserrat">${datamed[num].prodWithConc} \n \n</span>
-                <span id="just-line-break" style="font-family:Montserrat-Bold"> Apresentação: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat-Bold">✓ Apresentação: \n</span>
                 <span id="just-line-break" style="font-family:Montserrat">${datamed[num].tx_apres} \n \n</span>  
-                <span id="just-line-break" style="font-family:Montserrat-Bold"> Classe terapêutica: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat-Bold">✓ Classe terapêutica: \n</span>
                 <span id="just-line-break" style="font-family:Montserrat">${datamed[num].tx_class_terap} \n \n</span>
-                <span id="just-line-break" style="font-family:Montserrat-Bold"> Tarja: \n</span>
-                <span id="just-line-break" style="font-family:Montserrat">${datamed[num].tx_tarja} \n \n</span>
+                <span id="just-line-break" style="font-family:Montserrat-Bold">✓ Tarja: \n</span>
+                <span id="just-line-break" style="font-family:Montserrat">${datamed[num].tx_tarja} \n</span>
                 <button class="borderlessButton" onclick='prevMed()'>◀</button>
                 <span id="just-line-break" style="font-family:Montserrat">${num}</span>
                 <button class="borderlessButton" onclick='num++; createCard()'>▶</button>`
+    }
     return card;
 }
 
